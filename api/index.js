61e2d467cd7ba395aaac94e69158da455219985a
tomyrodeghiero <tomyrodeghiero@gmail.com>
2023-03-07 21:49:36 -0300

    Update server

--- api/index.js
+++ api/index.js
@@ -9,8 +9,6 @@ const bcrypt = require("bcryptjs");
 const app = express();
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-const multer = require("multer");
-const uploadMiddleware = multer({ dest: "uploads/" });
 const fs = require("fs");
 
 const salt = bcrypt.genSaltSync(10);
