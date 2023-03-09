19249081e32bc1670c787e5e0411d119e1b7dc22
tomyrodeghiero <tomyrodeghiero@gmail.com>
2023-03-08 21:28:09 -0300

    Update index.js server

--- api/index.js
+++ api/index.js
@@ -42,6 +42,55 @@ app.get("/api/post", async (req, res) => {
   res.json(await Post.find());
 });
 
+app.get("/post/:id", async (req, res) => {
+  const { id } = req.params;
+  const postDoc = await Post.findById(id).populate("author", ["username"]);
+  res.json(postDoc);
+});
+
+app.listen(4000);
+
+// const express = require("express");
+// const cors = require("cors");
+// const { default: mongoose, Schema } = require("mongoose");
+// mongoose.set("strictQuery", false);
+// const UserModel = require("./models/User");
+// const User = require("./models/User");
+// const Post = require("./models/Post");
+// const bcrypt = require("bcryptjs");
+// const app = express();
+// const jwt = require("jsonwebtoken");
+// const cookieParser = require("cookie-parser");
+// const multer = require("multer");
+// const uploadMiddleware = multer({ dest: "uploads/" });
+// const fs = require("fs");
+
+// const salt = bcrypt.genSaltSync(10);
+// const secret = "qiweoqwjoe123";
+
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: [
+//       "http://localhost:3000",
+//       "http://localhost:4173",
+//       "http://localhost:5173",
+//       "https://blog-v1-digf.onrender.com",
+//       "https://bienaventuradas-en-cristo.vercel.app",
+//       "https://blog-frontend-v2.onrender.com",
+//       "https://bienaventuradas-en-cristo-rest-api.vercel.app",
+//       "https://blog-project-red-seven.vercel.app",
+//     ],
+//   })
+// );
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use("/api/uploads", express.static(__dirname + "/uploads"));
+
+// mongoose.connect(
+//   "mongodb+srv://blog:blog-rest-api@cluster0.xih2rrz.mongodb.net/?retryWrites=true&w=majority"
+// );
+
 // app.post("/register", async (req, res) => {
 //   const { username, password } = req.body;
 //   try {
@@ -94,6 +143,54 @@ app.get("/api/post", async (req, res) => {
 //   res.cookie("token", "").json("okay");
 // });
 
+// app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
+//   try {
+//     const { originalname, path } = req.file;
+//     const parts = originalname.split(".");
+//     const ext = parts[parts.length - 1];
+//     const newPath = path + "." + ext;
+//     fs.renameSync(path, newPath);
+
+//     const { token } = req.cookies;
+
+//     if (!token) {
+//       return res.status(401).json({ message: "No token provided" });
+//     }
+
+//     jwt.verify(token, secret, {}, async (err, info) => {
+//       if (err) throw err;
+
+//       const { title, summary, content } = req.body;
+//       const postDoc = await Post.create({
+//         title,
+//         summary,
+//         content,
+//         cover: newPath,
+//         author: info.id,
+//       });
+//       res.json(postDoc);
+//     });
+//   } catch (error) {
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// });
+
+// app.put(
+//   "/post",
+//   (req, res, next) => {
+//     const { token } = req.cookies;
+//     if (!token) {
+//       res.status(401).json({ message: "Unauthorized" });
+//     } else {
+//       next();
+//     }
+//   },
+//   uploadMiddleware.single("file"),
+//   async (req, res) => {
+//     res.json(req.file);
+//   }
+// );
+
 // app.get("/post", async (req, res) => {
 //   res.json(
 //     await Post.find()
@@ -108,5 +205,3 @@ app.get("/api/post", async (req, res) => {
 //   const postDoc = await Post.findById(id).populate("author", ["username"]);
 //   res.json(postDoc);
 // });
-
-app.listen(4000);
