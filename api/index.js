b83d9e0593f629ea6e25e8823fe2b2ddeb5fa15a
tomyrodeghiero <tomyrodeghiero@gmail.com>
2023-03-08 21:46:37 -0300

    Update api

--- api/index.js
+++ api/index.js
@@ -9,6 +9,10 @@ const bcrypt = require("bcryptjs");
 const app = express();
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const multer = require("multer");
+const uploadMiddleware = multer({ dest: "uploads/" });
+const fs = require("fs");
+
 const salt = bcrypt.genSaltSync(10);
 const secret = "qiweoqwjoe123";
 
@@ -49,24 +53,6 @@ app.get("/api/post/:id", async (req, res) => {
 });
 
 app.listen(4000);
-
-// const express = require("express");
-// const cors = require("cors");
-// const { default: mongoose, Schema } = require("mongoose");
-// mongoose.set("strictQuery", false);
-// const UserModel = require("./models/User");
-// const User = require("./models/User");
-// const Post = require("./models/Post");
-// const bcrypt = require("bcryptjs");
-// const app = express();
-// const jwt = require("jsonwebtoken");
-// const cookieParser = require("cookie-parser");
-// const multer = require("multer");
-// const uploadMiddleware = multer({ dest: "uploads/" });
-// const fs = require("fs");
-
-// const salt = bcrypt.genSaltSync(10);
-// const secret = "qiweoqwjoe123";
 
 // app.use(
 //   cors({
