a4fde04c524a2ce8516ff60db931f6975be05c85
tomyrodeghiero <tomyrodeghiero@gmail.com>
2023-03-09 21:41:50 -0300

    Comment post

--- api/index.js
+++ api/index.js
@@ -9,9 +9,9 @@ const bcrypt = require("bcryptjs");
 const app = express();
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-const multer = require("multer");
-const uploadMiddleware = multer({ dest: "uploads/" });
-const fs = require("fs");
+// const multer = require("multer");
+// const uploadMiddleware = multer({ dest: "uploads/" });
+// const fs = require("fs");
 
 const salt = bcrypt.genSaltSync(10);
 const secret = "qiweoqwjoe123";
@@ -65,28 +65,27 @@ app.get("/api/profile", (req, res) => {
 });
 
 // POST
+// app.post("/api/post", uploadMiddleware.single("file"), async (req, res) => {
+//   const { originalname, path } = req.file;
+//   const parts = originalname.split(".");
+//   const ext = parts[parts.length - 1];
+//   const newPath = path + "." + ext;
+//   fs.renameSync(path, newPath);
 
-app.post("/api/post", uploadMiddleware.single("file"), async (req, res) => {
-  const { originalname, path } = req.file;
-  const parts = originalname.split(".");
-  const ext = parts[parts.length - 1];
-  const newPath = path + "." + ext;
-  fs.renameSync(path, newPath);
-
-  const { token } = req.cookies;
-  jwt.verify(token, secret, {}, async (err, info) => {
-    if (err) throw err;
-    const { title, summary, content } = req.body;
-    const postDoc = await Post.create({
-      title,
-      summary,
-      content,
-      cover: newPath,
-      author: info.id,
-    });
-    res.json(postDoc);
-  });
-});
+//   const { token } = req.cookies;
+//   jwt.verify(token, secret, {}, async (err, info) => {
+//     if (err) throw err;
+//     const { title, summary, content } = req.body;
+//     const postDoc = await Post.create({
+//       title,
+//       summary,
+//       content,
+//       cover: newPath,
+//       author: info.id,
+//     });
+//     res.json(postDoc);
+//   });
+// });
 
 app.post("/api/login", async (req, res) => {
   const { username, password } = req.body;
